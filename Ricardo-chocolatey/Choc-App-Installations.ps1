a51bbc879e7f437928817102f330236d4712670d
Ricardo <rfdz12@hotmail.com>
2023-02-06 15:56:18 -0800

    Choc-app-installations-added

---
+++ Ricardo-chocolatey/Choc-App-Installations.ps1
@@ -0,0 +1,53 @@
+## https://chocolatey.org/about
+
+## https://community.chocolatey.org/packages?q=Nuget
+ 
+## https://chocolatey.org/install
+
+Set-ExecutionPolicy Bypass -Scope Process -Force; [System.Net.ServicePointManager]::SecurityProtocol = [System.Net.ServicePointManager]::SecurityProtocol -bor 3072; iex ((New-Object System.Net.WebClient).DownloadString('https://community.chocolatey.org/install.ps1'))
+
+choco install sql-server-management-studio --version 15.0.18424.0 -y
+choco install virtualbox --version 6.1.34 -y
+choco install sqlsentryplanexplorer --version 2.0.0.2 -y
+choco install planexplorerssmsaddin --version 2.0 -y
+choco install mobaxterm --version 21.5.0 -y
+choco install studio3t --version 2022.6.1 -y
+choco install mongodb-compass --version 1.32.2 -y
+choco install windirstat --version 1.1.2.20161210 -y
+choco install winscp --version 5.21.1 -y
+choco install notepadplusplus --version 8.4.2 -y
+choco install keepass --version 2.51.1 -y
+choco install powershellplus --version 11.50.0.42619 -y
+choco install sqltoolbelt --version 2022.06.28 -y
+
+##
+## SSMS - https://docs.microsoft.com/en-us/sql/ssms/download-sql-server-management-studio-ssms?view=sql-server-ver16
+##
+## https://www.virtualbox.org/wiki/Downloads
+##
+## https://www.sentryone.com/plan-explorer
+##
+## https://windirstat.net/download.html
+##
+## https://mobaxterm.mobatek.net/download-home-edition.html
+##
+## https://studio3t.com/download-thank-you/?OS=win64
+##
+## https://www.mongodb.com/try/download/compass  
+##
+## https://winscp.net/eng/download.php
+##
+## https://notepad-plus-plus.org/downloads/
+##
+## https://keepass.info/download.html
+##
+## https://www.idera.com/productssolutions/freetools/powershellplus
+##
+## https://www.idera.com/dboptimizer-sql-database-optimization
+##
+## https://www.idera.com/productssolutions/freetools
+##
+## https://sourceforge.net/projects/spreadshcompare/
+##
+## https://portal.red-gate.com/
+##
