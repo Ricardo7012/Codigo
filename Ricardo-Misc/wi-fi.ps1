e4787a62d9f55794339b4ec34201ca2551a59811
Ricardo <124125896+Ricardo7012@users.noreply.github.com>
2024-04-23 20:33:21 -0700

    Update wi-fi.ps1
    Signed-off-by: Ricardo <124125896+Ricardo7012@users.noreply.github.com>

--- Ricardo-Misc/wi-fi.ps1
+++ Ricardo-Misc/wi-fi.ps1
@@ -2,11 +2,11 @@
 
 ipconfig
 
-nmap -sn 192.168.227.0/24
+nmap -sn 192.168.00.0/24
 
-nmap -sn 10.215.252.0/24
+nmap -sn 10.215.00.0/24
 
-nmap -sn 192.168.29.0/24
+nmap -sn 192.168.00.0/24
 
 arp -a
 
@@ -22,80 +22,3 @@ netsh wlan show drivers
 netsh wlan show wirelesscapabilities
 
 ##-nmap -sn 10.215.252.0/24 and ##-nmap -sn 192.168.227.0/24  
-
-##-MAC Address: 48:F8:B3:F4:38:B1 (Cisco-Linksys) R12
-##-MAC Address: 9C-B6-D0-D1-AF-FD (Killer Wireless-n/a/ac 1535 Wireless Network Adapter)i7Alien
-##-MAC Address: 18-DB-F2-4C-A1-D9 (Killer E2400 Gigabit Ethernet Controller) i7Alien
-##-MAC Address: C8:FF:77:E4:1A:43 (Dyson Limited) NM7-US-JNA2501A (10.215.252.129)
-##-MAC Address: C8:FF:77:DB:AC:E0 (Dyson Limited) SV8-US-JBA2352A
-##-MAC Address: D4:AD:FC:C6:14:E4 (Shenzhen Intellirocks Tech)
-##-MAC Address: 48:D6:D5:8E:6C:60 (Google) Google Home
-##-MAC Address: C0:95:6D:9F:A7:09 (Apple) AppleTV
-##-MAC Address: AC:BC:B5:DD:FD:D0 (Apple) Appleone
-##-MAC Address: 1E:ED:7D:C0:65:E1 (iPhone)
-##-MAC Address: 66:C6:FC:9A:C9:2F (iPad-12-Ricardo)
-##-MAC Address: 14:49:E0:0A:D0:12 (Samsung Electro-mechanics(Thailand))TV
-##-MAC Address: 5C:47:5E:1B:B9:B0 (Ring 2)
-##-MAC Address: 5C:47:5E:1B:B8:C4 (Ring 1)
-##-MAC Address: 00:0E:58:18:5D:FC (Sonos)SonosZBridge
-##-MAC Address: 00:0E:58:BA:49:1D (Sonos)
-##-MAC Address: 00:0E:58:FE:9B:DA (Sonos)		
-##-MAC Address: 00:0E:58:9A:9B:E4 (Sonos)	
-##-MAC Address: 00:0E:58:FD:67:6E (Sonos)
-##-MAC Address: 0C:47:C9:0F:F6:DC (Amazon Technologies) Fire
-##-MAC Address: 14:91:38:F4:15:E8 (Amazon Technologies) Echo Dot
-##-MAC Address: 38:1A:52:FF:0B:FA (Seiko Epson) Projector
-##-MAC Address: 2C:F4:32:02:7A:60 (Smart Plug)
-##-MAC Address: 2C:F4:32:1B:5F:7F (Smart Plug)
-##-MAC Address: 2C:F4:32:1A:B8:FF (Smart Plug)
-##-MAC Address: 2C:F4:32:02:8E:34 (Smart Plug)
-##-MAC Address: 6E:94:7F:42:BB:A0 ()
-##-MAC Address: D4:AD:FC:6C:14:E4 (Smart Kettle)
-##-MAC Address: F6:50:44:B8:60:5A
-
-
-
-##-MAC Address: F8-E4-3B-73-30-4B (ASIX USB to Gigabit Ethernet Family Adapter)i7Alien
-##-MAC Address: 9E-B6-D0-D1-AF-FD (Microsoft Wi-Fi Direct Virtual Adapter #3)
-##-MAC Address: AE-B6-D0-D1-AF-FD (Microsoft Wi-Fi Direct Virtual Adapter #4)
-##-MAC Address: 9C-B6-D0-D1-AF-FE (Bluetooth Device)
-
-##-MAC Address: 00-50-56-C0-00-01 (VMware)VMnet1
-##-MAC Address: 00-50-56-C0-00-08 (VMware)VMnet8
-##-MAC Address: 00:50:56:F0:9A:63 (VMware)
-##-MAC Address: 00:50:56:FE:CB:BA (VMware)
-##-MAC Address: 2C:F4:32:02:8E:34 (Espressif)
-##-MAC Address: 2C:F4:32:1B:5F:7F (Espressif)
-
-
-
-## 78:2B:46:C6:5B:D3
-## 7C:2A:CA:B8:69:4B
-## 14:91:38:F4:15:E8
-## 9C:B6:D0:D1:AF:FD
-## 0C:47:C9:0F:F6:DC
-## C8:FF:77:E4:1A:43
-## C8:FF:77:DB:AC:E0
-## 38:1A:52:FF:0B:FA
-## 2C:F4:32:02:7A:60
-## 2C:F4:32:02:8E:34
-## 2C:F4:32:1A:B8:FF
-## 2C:F4:32:1B:5F:7F
-## 48:D6:D5:8E:6C:60
-## 4A:82:B3:60:41:51
-## F6:50:44:B8:60:5A
-## B8:81:98:B1:D9:AB
-## 18:DB:F2:4C:A1:D9
-## 44:E5:17:5D:55:48
-## 50:1A:C5:E6:7E:09
-## 5C:47:5E:1B:B8:C4
-## 5C:47:5E:1B:B9:B0
-## 04:7B:CB:20:D5:E6
-## 00:0E:58:18:5D:FC
-## 00:0E:58:BA:49:1D
-## B8:2C:A0:98:15:63
-## B8:2C:A0:98:15:A6
-## 00:0E:58:FE:9B:DA
-## B4:C9:B9:6B:92:26
-## 02:50:F2:37:5D:4B
-## C4:9D:ED:A2:1E:5B
