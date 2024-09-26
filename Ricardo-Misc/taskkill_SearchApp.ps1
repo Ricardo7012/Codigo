4c30897dde352e8b4f159d950f3848357dff5526
Ricardo <rfdz12@hotmail.com>
2024-09-26 10:14:28 -0700

    update

--- Ricardo-Misc/taskkill_SearchApp.ps1
+++ Ricardo-Misc/taskkill_SearchApp.ps1
@@ -2,3 +2,11 @@
 cd C:\Windows\SystemApps> 
 taskkill /f /im SearchApp.exe
 # move Microsoft.Windows.Search_cw5n1h2txyewy Microsoft.Windows.Search_cw5n1h2txyewy.old
+
+## REVERSE IT 
+
+## Start-Process -FilePath "C:\Windows\SystemApps\SearchApp.exe"
+
+Start-Process -FilePath .\Windows\SystemApps\searchapp.exe
+
+Add-AppxPackage -Path "C:\Windows\SystemApps\Microsoft.Windows.Search_cw5n1h2txyewy\Appxmanifest.xml" -DisableDevelopmentMode -Register
